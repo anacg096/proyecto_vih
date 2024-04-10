05ee300b8415fc6b53c9f72931c21956f8ccaa2c
anacg096 <anacilleguti@gmail.com>
2024-04-10 16:46:19 +0200

    se corrige una línea del footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,7 +10,6 @@
 
 <body>
     <footer class="footer margen">
-        <hr />
         <div class="footer__eresVihda">
             <img src="{{ asset('assets/images/shared/footer/image_2.png') }}" alt="icono_eresVIHDA" />
         </div>
